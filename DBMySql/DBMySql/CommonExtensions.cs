3d9ff9eb1063004bb9591f59962b91ddb103590a
Bret Dalldorf <bret.dalldorf@gmail.com>
2018-12-16 18:38:29 -0800

    feat(app): Added base MySql methods

---
+++ DBMySql/DBMySql/CommonExtensions.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Reflection;
+using System.Linq;
+using System.ComponentModel.DataAnnotations;
+using DBMySql;
+
+#region Attributes
+
+public class TableFieldNameAttribute : Attribute
+{
+    public string FieldName { get; }
+
+    internal TableFieldNameAttribute(string fieldName)
+    {
+        if (FieldName == null) FieldName = string.Empty;
+
+        FieldName = fieldName;
+    }
+}
+
+public class TableFieldExcludeFromUpdateAttribute : Attribute
+{
+    public bool ExcludeFromUpdate { get; }
+
+    internal TableFieldExcludeFromUpdateAttribute(bool excludeFromUpdate)
+    {
+        ExcludeFromUpdate = excludeFromUpdate;
+    }
+}
+
+public class TableFieldExcludeFromInsertAttribute : Attribute
+{
+    public bool ExcludeFromInsert { get; }
+
+    internal TableFieldExcludeFromInsertAttribute(bool excludeFromInsert)
+    {
+        ExcludeFromInsert = excludeFromInsert;
+    }
+}
+
+public class TableNameAttribute : Attribute
+{
+    public string TableName { get; }
+
+    internal TableNameAttribute(string tableName)
+    {
+        if (TableName == null) TableName = string.Empty;
+
+        TableName = tableName;
+    }
+}
+
+#endregion
+
+#region Class Extensions
+public static class ClassExtension
+{
+    /// <summary>
+    /// Returns an empty string if the [TableName] attribute isn't added to the property
+    /// </summary>
+    public static string TableName(this IDatabaseModel value)
+    {
+        return value.GetType()
+                        .GetCustomAttribute<TableNameAttribute>()
+                        .TableName;
+    }
+}
+
+#endregion
+
+#region Integer Extensions
+
+public static class IntegerExtension
+{
+    /// <summary>
+    /// Returns an empty string if the [TableFieldName] attribute isn't added to the property
+    /// </summary>
+    public static string TableField(this int value)
+    {
+        return value.GetType()
+                        .GetMember(value.ToString())
+                        .First()
+                        .GetCustomAttribute<TableFieldNameAttribute>()
+                        .FieldName;
+    }
+
+    public static string ExcludeFromUpdate(this bool value)
+    {
+        return value.GetType()
+                        .GetMember(value.ToString())
+                        .First()
+                        .GetCustomAttribute<TableFieldNameAttribute>()
+                        .FieldName;
+    }
+
+    public static bool IsEmpty(this int value) => value == SQLiteDBCommon.EmptyInt ? true : false;
+}
+#endregion
+
+#region String Extensions
+
+public static class StringExtension
+{
+    /// <summary>
+    /// Returns an empty string if the [TableFieldName] attribute isn't added to the property
+    /// </summary>
+    public static string TableField(this string value)
+    {
+        return value.GetType()
+                        .GetMember(value)
+                        .First()
+                        .GetCustomAttribute<TableFieldNameAttribute>()
+                        .FieldName;
+    }
+
+    public static bool ExcludeFromUpdate(this bool value)
+    {
+        return value.GetType()
+                        .GetCustomAttribute<TableFieldExcludeFromUpdateAttribute>()
+                        .ExcludeFromUpdate;
+    }
+
+    public static bool ExcludeFromInsert(this bool value)
+    {
+        return value.GetType()
+                        .GetCustomAttribute<TableFieldExcludeFromInsertAttribute>()
+                        .ExcludeFromInsert;
+    }
+
+
+
+    public static bool IsEmpty(this String value) => value == SQLiteDBCommon.EmptyString ? true : false;
+}
+
+#endregion
+
+#region  Enumerations Extensions
+
+public static class EnumerationExtension
+{
+    public static string Description(this Enum enumValue)
+    {
+        return enumValue.GetType()
+                        .GetMember(enumValue.ToString())
+                        .First()
+                        .GetCustomAttribute<DisplayAttribute>()
+                        .GetDescription();
+    }
+
+    public static string TableFieldName(this Enum enumValue)
+    {
+        return enumValue.GetType()
+                        .GetMember(enumValue.ToString())
+                        .First()
+                        .GetCustomAttribute<TableFieldNameAttribute>()
+                        .FieldName;
+    }
+}
+
+    #endregion
---
+++ DBMySql/DBMySql/MySQLDBStateless.cs
@@ -0,0 +1,291 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Reflection;
+using System.Text;
+using System.Linq;
+using MySql.Data.MySqlClient;
+
+namespace DBMySql
+{
+    public static class SQLiteDBStateless
+    {
+        private static MySqlConnection OpenConnection()
+        {
+            MySqlConnection MySqlConnection = new MySqlConnection("connectionString");
+
+            MySqlConnection.Open();
+
+            return MySqlConnection;
+        }
+
+        private static MySqlTransaction BeginTransaction()
+        {
+            return OpenConnection().BeginTransaction();
+        }
+
+        private static MySqlCommand Command(string sql, MySqlConnection mysqlConnection)
+        {
+            return new MySqlCommand(sql, mysqlConnection);
+        }
+
+        private static MySqlCommand Command(string sql, MySqlTransaction mysqlTransaction)
+        {
+            return new MySqlCommand(sql, mysqlTransaction.Connection, mysqlTransaction);
+        }
+
+        private static MySqlDataAdapter DataAdapter(string sql, MySqlConnection mysqlConnection)
+        {
+            return new MySqlDataAdapter(sql, mysqlConnection);
+        }
+
+        public static int ExecNonQuery(string sql)
+        {
+            int Results = -1;
+            MySqlConnection MySqlConnection = null;
+
+            try
+            {
+                MySqlConnection = OpenConnection();
+
+                Results = Command(sql, MySqlConnection).ExecuteNonQuery();
+            }
+            catch (Exception exception)
+            {
+            }
+            finally
+            {
+                if (MySqlConnection != null)
+                    MySqlConnection.Close();
+            }
+
+            return Results;
+        }
+
+        public static bool ExecNonQueryTransaction(List<string> sqlStatements)
+        {
+            MySqlTransaction MySqlTransaction = null;
+            StringBuilder l_Results = new StringBuilder();
+
+            try
+            {
+                MySqlTransaction = BeginTransaction();
+
+                foreach (string sqlStatement in sqlStatements)
+                {
+                    Command(sqlStatement, MySqlTransaction).ExecuteNonQuery();
+                }
+            }
+            catch (Exception exception)
+            {
+                if (MySqlTransaction != null)
+                {
+                    MySqlTransaction.Rollback();
+                    MySqlTransaction.Dispose();
+                    MySqlTransaction = null;
+                    return false;
+                }
+            }
+            finally
+            {
+                if (MySqlTransaction != null)
+                {
+                    MySqlTransaction.Commit();
+                }
+            }
+
+            return true;
+        }
+
+        public static long ExecInsertNonQueryReturnID(string sql)
+        {
+            long RowID = MySQLDBCommon.EmptyLong;
+            MySqlTransaction MySqlTransaction = null;
+            StringBuilder l_Results = new StringBuilder();
+
+            try
+            {
+                MySqlTransaction = BeginTransaction();
+
+                MySqlCommand MySqlCommand = Command(sql, MySqlTransaction);
+                MySqlCommand.ExecuteNonQuery();
+
+                RowID = MySqlCommand.LastInsertedId;
+            }
+            catch (Exception exception)
+            {
+                if (MySqlTransaction != null)
+                {
+                    MySqlTransaction.Rollback();
+                    MySqlTransaction.Dispose();
+                    MySqlTransaction = null;
+                }
+            }
+            finally
+            {
+                if (MySqlTransaction != null)
+                {
+                    MySqlTransaction.Commit();
+                }
+            }
+
+            return RowID;
+        }
+
+        public static object ExecScalar(string sql)
+        {
+            {
+                MySqlConnection MySqlConnection = null;
+                object ScalarReturnObject = null;
+
+                try
+                {
+                    MySqlConnection = OpenConnection();
+
+                    ScalarReturnObject = Command(sql, MySqlConnection).ExecuteScalar();
+                }
+                catch (Exception exception)
+                {
+                }
+                finally
+                {
+                    if (MySqlConnection != null)
+                        MySqlConnection.Close();
+                }
+
+                return ScalarReturnObject;
+            }
+        }
+
+        public static MySqlDataReader ExecDataReader(string sql)
+        {
+            MySqlConnection MySqlConnection = null;
+            MySqlDataReader MySqlDataReader = null;
+
+            try
+            {
+                MySqlConnection = OpenConnection();
+
+                MySqlDataReader = Command(sql, MySqlConnection).ExecuteReader();
+            }
+            catch (Exception exception)
+            {
+            }
+            finally
+            {
+                if (MySqlConnection != null)
+                    MySqlConnection.Close();
+            }
+
+            return MySqlDataReader;
+        }
+
+        public static DataTable ExecDataTable(string sql)
+        {
+            {
+                MySqlConnection MySqlConnection = null;
+                MySqlDataAdapter MySqlDataAdapter = null;
+                DataTable DataTable = new DataTable();
+
+                try
+                {
+                    MySqlConnection = OpenConnection();
+
+                    MySqlDataAdapter = DataAdapter(sql, MySqlConnection);
+                    MySqlDataAdapter.Fill(DataTable);
+                }
+                catch (Exception exception)
+                {
+                }
+                finally
+                {
+                    if (MySqlConnection != null)
+                        MySqlConnection.Close();
+                }
+
+                return DataTable;
+            }
+        }
+
+        public static string ModelFieldNames(Type model)
+        {
+            StringBuilder StringBuilder = new StringBuilder();
+
+            foreach (FieldInfo Field in model.GetFields(System.Reflection.BindingFlags.Public
+                | System.Reflection.BindingFlags.GetField | BindingFlags.Instance))
+            {
+                string Value = (string)Field.CustomAttributes.Where(customAttributes => customAttributes.AttributeType == typeof(TableFieldNameAttribute)).First().ConstructorArguments.First().Value;
+                StringBuilder.Append(StringBuilder.Length == 0 ? $"{Value}" : $", {Value}");
+            }
+
+            return StringBuilder.ToString();
+        }
+
+        public static string ModelFieldValues(object model)
+        {
+            StringBuilder StringBuilder = new StringBuilder();
+
+            foreach (var properties in model.GetType().GetFields(System.Reflection.BindingFlags.Public
+                | System.Reflection.BindingFlags.GetField | BindingFlags.Instance))
+            {
+                object Value = properties.GetValue(model);
+                StringBuilder.Append(StringBuilder.Length == 0 ? $"{MySQLDBCommon.SetValueForSql(Value)}" : $", {MySQLDBCommon.SetValueForSql(Value)}");
+            }
+
+            return StringBuilder.ToString();
+        }
+
+        public static string GenerateInsertFields(IDatabaseModel model)
+        {
+            StringBuilder StringBuilderFields = new StringBuilder();
+            StringBuilder StringBuilderValues = new StringBuilder();
+
+            foreach (FieldInfo Field in model.GetType().GetFields(System.Reflection.BindingFlags.Public
+                | System.Reflection.BindingFlags.GetField | BindingFlags.Instance))
+            {
+                CustomAttributeData l_ExcludeFromUpdate = Field.CustomAttributes.FirstOrDefault(customAttributes => customAttributes.AttributeType == typeof(TableFieldExcludeFromUpdateAttribute));
+
+                if (l_ExcludeFromUpdate != null && Convert.ToBoolean(l_ExcludeFromUpdate.ConstructorArguments.First().Value))
+                    continue;
+
+                CustomAttributeData l_TableFieldName = Field.CustomAttributes.FirstOrDefault(customAttributes => customAttributes.AttributeType == typeof(TableFieldNameAttribute));
+
+                if (l_TableFieldName == null)
+                    continue;
+
+                string FieldName = (string)l_TableFieldName.ConstructorArguments.First().Value;
+                object FieldValue = Field.GetValue(model);
+
+                StringBuilderFields.Append(StringBuilderFields.Length == 0 ? $"({FieldName}" : $", {FieldName}");
+                StringBuilderValues.Append(StringBuilderValues.Length == 0 ? $"{MySQLDBCommon.SetValueForSql(FieldValue)}" : $", {MySQLDBCommon.SetValueForSql(FieldValue)}");
+            }
+
+            return StringBuilderFields.Append($") VALUES ({StringBuilderValues.ToString()})").ToString();
+        }
+
+        public static string GenerateUpdateFields(IDatabaseModel model)
+        {
+            StringBuilder StringBuilder = new StringBuilder();
+
+            foreach (FieldInfo Field in model.GetType().GetFields(System.Reflection.BindingFlags.Public
+                  | System.Reflection.BindingFlags.GetField | BindingFlags.Instance))
+            {
+                CustomAttributeData l_ExcludeFromUpdate = Field.CustomAttributes.FirstOrDefault(customAttributes => customAttributes.AttributeType == typeof(TableFieldExcludeFromUpdateAttribute));
+
+                if (l_ExcludeFromUpdate != null && Convert.ToBoolean(l_ExcludeFromUpdate.ConstructorArguments.First().Value))
+                    continue;
+
+                CustomAttributeData l_TableFieldName = Field.CustomAttributes.FirstOrDefault(customAttributes => customAttributes.AttributeType == typeof(TableFieldNameAttribute));
+
+                if (l_TableFieldName == null)
+                    continue;
+
+                string FieldName = (string)l_TableFieldName.ConstructorArguments.First().Value;
+                object FieldValue = Field.GetValue(model);
+                StringBuilder.Append(StringBuilder.Length == 0 ? $"{FieldName} = {MySQLDBCommon.SetValueForSql(FieldValue)}" : $", {FieldName} = {MySQLDBCommon.SetValueForSql(FieldValue)}");
+            }
+
+            return StringBuilder.ToString();
+        }
+    }
+}
