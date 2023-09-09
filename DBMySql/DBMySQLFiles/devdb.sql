179b2c9640efa378e14e4161a9a1be67e7b51b08
bdalldorf <bret.dalldorf@gmali.com>
2023-09-09 03:55:55 -0700

    feat(app): Updated devdb.sql File

--- DBMySql/DBMySQLFiles/devdb.sql
+++ DBMySql/DBMySQLFiles/devdb.sql
@@ -1,15 +1,15 @@
 CREATE DATABASE  IF NOT EXISTS `devdb` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
 USE `devdb`;
--- MySQL dump 10.13  Distrib 8.0.16, for Win64 (x86_64)
+-- MySQL dump 10.13  Distrib 8.0.34, for Win64 (x86_64)
 --
 -- Host: localhost    Database: devdb
 -- ------------------------------------------------------
--- Server version	8.0.16
+-- Server version	8.0.34
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
- SET NAMES utf8 ;
+/*!50503 SET NAMES utf8 */;
 /*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
 /*!40103 SET TIME_ZONE='+00:00' */;
 /*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
@@ -23,9 +23,9 @@ USE `devdb`;
 
 DROP TABLE IF EXISTS `usruser_usr`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
- SET character_set_client = utf8mb4 ;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `usruser_usr` (
-  `usrID` int(11) NOT NULL AUTO_INCREMENT,
+  `usrID` int NOT NULL AUTO_INCREMENT,
   `usrUID` varchar(45) NOT NULL,
   `usrFirstName` varchar(100) NOT NULL,
   `usrLastName` varchar(100) NOT NULL,
@@ -34,7 +34,7 @@ CREATE TABLE `usruser_usr` (
   UNIQUE KEY `usrUID_UNIQUE` (`usrUID`),
   UNIQUE KEY `usrID_UNIQUE` (`usrID`),
   UNIQUE KEY `usrEmailAddress_UNIQUE` (`usrEmailAddress`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -43,12 +43,9 @@ CREATE TABLE `usruser_usr` (
 
 LOCK TABLES `usruser_usr` WRITE;
 /*!40000 ALTER TABLE `usruser_usr` DISABLE KEYS */;
+INSERT INTO `usruser_usr` VALUES (1,'Tester1UID','Tester1FirstName','Tester1LastName','tester1email@test.com');
 /*!40000 ALTER TABLE `usruser_usr` ENABLE KEYS */;
 UNLOCK TABLES;
-
---
--- Dumping routines for database 'devdb'
---
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -59,4 +56,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2023-09-09  1:07:29
+-- Dump completed on 2023-09-09  3:53:16
